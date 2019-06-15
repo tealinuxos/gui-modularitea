5477faee729d672b9067d7a8066fbaa475b73e85
saifoelloh <saifoelloh@gmail.com>
2019-06-16 01:51:05 +0700

    build from scratch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import { 
+  Row,
+  Col,
+ } from "reactstrap";
+import Hero from './components/Hero';
+import Module from './components/Module';
+
+const appList = [
+  {
+    name: 'Web Development',
+    url: 'localhost:3333/api/web',
+    background: '',
+  },
+  {
+    name: 'UI Designer',
+    url: 'localhost:3333/api/multimedia',
+    background: '',
+  },
+  {
+    name: 'Mobile Apps',
+    url: 'localhost:3333/api/mobile',
+    background: '',
+  },
+];
+
 
 function App() {
+  useEffect(() => console.log(appList),[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-100">
+      <div className="d-flex flex-column align-content-stretch" style={{height: '100%'}}>
+        <Row className="m-0">
+          <Col xl="12" lg="12" md="12" sm="12" className="p-0">
+            <Hero />
+          </Col>
+        </Row>
+        <Row className="w-100 m-0 flex-fill">
+          <Col xl="12" lg="12" md="12" sm="12" className="p-0">
+            <Row className="h-100 m-0">
+              {appList.map((datum, key) => {
+                return <Module key={key} data={datum} />
+              })}
+            </Row>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
